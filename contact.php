14964ab54140a94686398308f4aa4a3c7c1a6f1d
jeanvay <nonhumain@protonmail.com>
2021-07-23 11:43:20 +0200

    creation et mise en place de la page contact

---
+++ contact.php
@@ -0,0 +1,34 @@
+<?php
+require_once 'core/core.php';
+include 'includes/header.php';
+
+?>
+
+<div class="page_contact">
+
+    <div class="bloc1">
+            <h2>
+                muriel.coutellier67@gmail.com <br>
+                +33777240109 <br>
+                Instagram
+            </h2>
+    </div>
+    <div class="bloc2">
+            <h1>
+            Prise <br>
+            de contact
+            </h1>
+    </div>
+
+</div>
+
+<script>
+    document.body.style.backgroundColor="#9A4747";
+
+    let bouton = document.querySelector(".bouton_desktop");
+    bouton.style.backgroundColor="#FCF7EC";
+
+    let boutonText= document.querySelector(".bouton_desktop a");
+    boutonText.style.color="#9A4747";
+ 
+</script>
