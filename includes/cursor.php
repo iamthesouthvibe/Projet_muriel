100f8bc893bc369574e40083b0199b832aaa17a4
iamthesouthvibe <leo.labeaume@hotmail.fr>
2021-07-23 10:24:10 +0200

    Update cursor.php

--- includes/cursor.php
+++ includes/cursor.php
@@ -1,6 +1,3 @@
-<style>
-
-</style>
 
 <div class="cursor"></div>
 <div class="cursor2"></div>
@@ -10,6 +7,7 @@
 var cursor = document.querySelector(".cursor");
 var cursor2 = document.querySelector(".cursor2");
 document.addEventListener("mousemove",function(e){
+  e.preventDefault();
   cursor.style.cssText = cursor2.style.cssText = "left: " + e.screenX + "px; top: " + e.screenY + "px;";
 });
 </script>
