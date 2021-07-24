79e5bf5a5d17bee593f25a59d334895f67bbc0a4
jeanvay <nonhumain@protonmail.com>
2021-07-24 12:53:24 +0200

    mise en place page maison

---
+++ Maison.php
@@ -0,0 +1,30 @@
+<?php
+require_once 'core/core.php';
+include 'includes/header.php';
+?>
+
+
+<main>
+
+<div class="entree_page">
+    <div class="div_h1_titre">
+        <h1 class="h1_titre">
+        Villa Grand Large
+Baie des Anses d’Arlet
+        </h1>
+    </div>
+    <div class="div_h2_titre"></div>
+</div>
+
+
+
+
+
+
+
+    <?php
+    include "includes/cursor.php";
+    include("./includes/footer.php"); 
+    ?>
+
+</main>
