1d0b1687f3e3491d3cde539a54f3c83179b78a1e
jeanvay <nonhumain@protonmail.com>
2021-08-27 19:58:06 +0200

    demarrage page produit unitaire

---
+++ products_unit.php
@@ -0,0 +1,29 @@
+<?php
+require_once 'core/core.php';
+include 'includes/header.php';
+?>
+
+
+<main>
+
+    <!-- Section accueil -->
+    <div class="qhero_page_produits_unit">
+        <div class="qhero_entree_page_produits_unit">
+            <div class="div_h1_titre">
+                <h1 class="h1_titre">
+                    Nom du produit
+                </h1>
+            </div>
+        </div>
+    </div>
+
+    <!-- Section Présentation du produit -->
+
+    <div class="section_presentation_produit"></div>
+
+
+    <?php
+    include "includes/cursor.php";
+    include("./includes/footer.php"); ?>
+
+</main>
