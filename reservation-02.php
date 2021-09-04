60018d53fd57d87af0b5c0a4d2abc09bc59a7f24
jeanvay <nonhumain@protonmail.com>
2021-09-04 12:27:45 +0200

    creation page reservation 02

---
+++ reservation-02.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'core/core.php';
+include 'includes/header.php';
+
+?>
+
+<main>
+
+
+</main>
+
+<script>
+    document.body.style.backgroundColor = "#9A4747";
+
+    let bouton = document.querySelector(".bouton_desktop");
+    bouton.style.display = "none";
+</script>
