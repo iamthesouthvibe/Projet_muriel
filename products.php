17d3a95139e922e7667faeaf54952bb142736148
iamthesouthvibe <abc.studiodecreation@gmail.com>
2021-09-21 20:33:50 +0200

    Update products.php

--- products.php
+++ products.php
@@ -5,45 +5,40 @@ include 'includes/header.php';
 $select = $db->query("SELECT * FROM products");
 ?>
 
-<main id="testFade">
 
-<svg id="fader"></svg>
-<script>
-            fadeInPage();
-        </script>
-    <!-- Section accueil -->
-    <div class="qhero_page_produits">
-        <div class="qhero_entree_page_produits">
-            <div class="div_h1_titre">
-                <h1 class="h1_titre">
-                    Nos Articles
-                </h1>
-            </div>
-            <div class="div_h3_text_presentation_produits">
-                <h3 class="h3_titre">
-                    Sed rutrum vulputate dapibus. Vivamus id tincidunt eros, in suscipit quam. Praesent aliquet justo auctor urna feugiat, luctus aliquam odio pharetra. Fusce iaculis mauris sem. Donec varius urna libero, sit amet tincidunt enim laoreet ut. Donec non sem pharetra, ullamcorper nibh in, lacinia risus. Proin rutrum egestas massa, a cursus diam interdum ac.
-                </h3>
-            </div>
+<!-- Section accueil -->
+<div class="qhero_page_produits">
+    <div class="qhero_entree_page_produits">
+        <div class="div_h1_titre">
+            <h1 class="h1_titre">
+                Nos Articles
+            </h1>
+        </div>
+        <div class="div_h3_text_presentation_produits">
+            <h3 class="h3_titre">
+                Sed rutrum vulputate dapibus. Vivamus id tincidunt eros, in suscipit quam. Praesent aliquet justo auctor urna feugiat, luctus aliquam odio pharetra. Fusce iaculis mauris sem. Donec varius urna libero, sit amet tincidunt enim laoreet ut. Donec non sem pharetra, ullamcorper nibh in, lacinia risus. Proin rutrum egestas massa, a cursus diam interdum ac.
+            </h3>
         </div>
     </div>
+</div>
 
-    <!-- Section boutons de filtre -->
+<!-- Section boutons de filtre -->
 
-    <div class="section_boutons_filtre">
-        <div class="boutons_maisons">
-            <button class="bouton_selection_maison"><a href="">Martinique</a></button>
-            <button class="bouton_selection_maison"><a href="">Pyrénnées</a></button>
-            <button class="bouton_selection_maison"><a href="">Gruissan</a></button>
-            <button class="bouton_selection_maison"><a href="">Narbonne</a></button>
-        </div>
-        <div class="bouton_articles">
-            <button class="bouton_selection_maison bouton_agrandie"><a href="">Tous nos Articles</a></button>
-        </div>
+<div class="section_boutons_filtre">
+    <div class="boutons_maisons">
+        <button class="bouton_selection_maison"><a href="">Martinique</a></button>
+        <button class="bouton_selection_maison"><a href="">Pyrénnées</a></button>
+        <button class="bouton_selection_maison"><a href="">Gruissan</a></button>
+        <button class="bouton_selection_maison"><a href="">Narbonne</a></button>
     </div>
+    <div class="bouton_articles">
+        <button class="bouton_selection_maison bouton_agrandie"><a href="">Tous nos Articles</a></button>
+    </div>
+</div>
 
-    <!-- Section produits -->
+<!-- Section produits -->
 
-    <div class="espace_flexbox_produit_02">
+<div class="espace_flexbox_produit_02">
     <?php while ($product = mysqli_fetch_assoc($select)) : ?>
         <div class="espace_produit_02">
             <a href="">
@@ -57,21 +52,14 @@ $select = $db->query("SELECT * FROM products");
             </a>
             <a href="" class="lien_achat">Acheter →</a>
         </div>
-        <?php endwhile; ?>
-    </div>
+    <?php endwhile; ?>
+</div>
 
 
 
-    <?php
-    include "includes/cursor.php";
-    include("./includes/footer.php"); ?>
+<?php
+include "includes/cursor.php";
+include("./includes/footer.php"); ?>
 
-    <script>
-        function fadeInPage() {
-            if (!window.AnimationEvent) { return; }
-            var fader = document.getElementById('fader');
-    fader.classList.add('fade-out');
-}
-    </script>
 
 </main>
