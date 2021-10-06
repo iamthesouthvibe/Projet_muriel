87eea0f1e8ee09182cb58c7b9f7b53058bbab70a
jeanvay <nonhumain@protonmail.com>
2021-10-06 13:01:09 +0200

    reponsive de la nouvelle page reservation confirmation aisni que diverses elements

---
+++ confirmation-reservation.php
@@ -0,0 +1,42 @@
+<?php
+require_once 'core/core.php';
+include 'includes/header.php';
+
+
+?>
+
+
+    
+        <div class="qhero_page_confirmation">
+            <h1 class="style_h1_confirmation">Réservation <br>envoyée !</h1>
+            <h2 class="style_h2_confirmation">Je reviens vers vous sous 3 jours par retour de mail pour confirmation et signature du contrat.</h2>
+        </div>
+        <div class="section_recapitulatif">
+            <h3 class="style_h3_confirmation_reservation">Récapitulatif</h3>
+            <p class="style_texte_recapitulatif">Jolie Maison de Ville <br>
+                23/02/2022 - 25/03/2022 <br>
+                4 Adultes <br>
+                2 Enfants <br>
+                Jean Vayssié <br>
+                23 rue du chatelet 74240 Gaillard <br>
+                jean.vayssie@orange.fr <br>
+                0777340108 <br>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Netus tincidunt lobortis habitant enim enim ornare ultricies sed pellentesque. Nunc, ut metus iaculis diam lectus leo. Vulputate fringilla nisi, felis tincidunt lobortis porta. Sit sagittis proin ut rhoncus, consectetur eu consectetur vulputate.
+            </p>
+        </div>
+        <div class="section_prix-total">
+            <h4>Prix Total</h4>
+            <h5>450€</h5>
+        </div>
+    
+
+
+
+
+
+
+
+<script>
+    document.getElementById('bouton_responsive').style.display = 'none';
+    document.getElementById('bouton_desktop').style.display = 'none';
+</script>
--- includes/header.php
+++ includes/header.php
@@ -27,7 +27,7 @@
         </a>
       </div>
       <div class="header_right">
-        <button class="bouton_desktop"><a href="reservation.php">Réserver</a></button>
+        <button class="bouton_desktop" id="bouton_desktop"><a href="reservation.php">Réserver</a></button>
         <div class="burger-menu">
           <span></span>
           <span></span>
