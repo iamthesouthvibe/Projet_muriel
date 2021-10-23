778aa04b250cb09d0ccb02c0fad2f545c5546eb5
jeanvay <40472316+jeanvay@users.noreply.github.com>
2021-10-23 17:25:08 +0200

    ajout des pages blog unit et blog ainsiq ue adapatation de certaines modifications mineures

---
+++ blog-unit.php
@@ -0,0 +1,68 @@
+<?php
+require_once 'core/core.php';
+include 'includes/header.php';
+?>
+
+<main>
+
+<!-- Section accueil -->
+<div class="qhero_page_blog-unit">
+        <div class="qhero_entree_page_blog-unit">
+            <div class="div_h1_titre_blog-unit">
+                <h1 class="h1_titre_blog-unit">
+                    Titre de l'article de blog
+                </h1>
+            </div>
+            <div class="div_h2_date_blog-unit">
+                <h2 class="h2_date_blog-unit">
+                    05/05/2014
+                </h2>
+            </div>
+            <div class="div_h3_text_presentation_blog-unit">
+                <h3 class="h3_titre-blog-unit">
+                    Sed rutrum vulputate dapibus. Vivamus id tincidunt eros, in suscipit quam. Praesent aliquet justo auctor urna feugiat, luctus aliquam odio pharetra. Fusce iaculis mauris sem. Donec varius urna libero, sit amet tincidunt enim laoreet ut. Donec non sem pharetra, ullamcorper nibh in, lacinia risus. Proin rutrum egestas massa, a cursus diam interdum ac.
+                </h3>
+            </div>
+        </div>
+    </div>
+
+    <!-- Section presentation maison -->
+    <div class="container_presentation_blog-unit">
+            <div class="container_presentation_blog-unit_colonne_gauche">
+                <div class="texte_gauche-blog-unit">
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus aliquet turpis, ut laoreet nisl imperdiet a. Sed at iaculis metus. Curabitur lobortis, neque porttitor tempus elementum, lectus tortor volutpat nisi, et consectetur tellus nunc eu turpis. Morbi rhoncus at nisi sed eleifend. Vivamus finibus nibh ipsum, et malesuada est ullamcorper nec. Phasellus pellentesque arcu at nunc tincidunt, id hendrerit arcu varius. Donec imperdiet condimentum lacus, ac maximus tortor tempus sit amet. Mauris tempor turpis ac sapien euismod feugiat. Nam at dolor a augue feugiat scelerisque non in sem. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris at est lacus. Nulla hendrerit, lectus ut vestibulum imperdiet, est dolor aliquet libero, at gravida magna lectus nec metus. Nam vitae finibus ante. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+
+
+                    </p>
+                    <div class="image_gauche-blog-unit">
+                        <img src="assets/jpg/images page d'accueil/Villa-grande-Anse-Martinique02.jpg" alt="Photo Maison Gauche">
+                    </div>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus aliquet turpis, ut laoreet nisl imperdiet a. Sed at iaculis metus. Curabitur lobortis, neque porttitor tempus elementum, lectus tortor volutpat nisi, et consectetur tellus nunc eu turpis. Morbi rhoncus at nisi sed eleifend. Vivamus finibus nibh ipsum, et malesuada est ullamcorper nec. Phasellus pellentesque arcu at nunc tincidunt, id hendrerit arcu varius. Donec imperdiet condimentum lacus, ac maximus tortor tempus sit amet. Mauris tempor turpis ac sapien euismod feugiat. Nam at dolor a augue feugiat scelerisque non in sem. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris at est lacus. Nulla hendrerit, lectus ut vestibulum imperdiet, est dolor aliquet libero, at gravida magna lectus nec metus. Nam vitae finibus ante. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+
+Maecenas ullamcorper condimentum gravida. Etiam efficitur ut mi at consequat. Nullam eu magna quis dui maximus vehicula. Ut semper, leo ut sagittis volutpat, leo felis suscipit risus, nec laoreet elit quam ac sapien. Donec vel purus mauris. Vivamus urna orci, porta sit amet odio eu, scelerisque posuere arcu. Donec dignissim magna vel rhoncus pulvinar. 
+                    </p>
+                </div>
+            </div>
+            <div class="container_presentation_blog-unit_colonne_droite">
+                <div class="texte-droite-blog-unit">
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus aliquet turpis, ut laoreet nisl imperdiet a. Sed at iaculis metus. Curabitur lobortis, neque porttitor tempus elementum, lectus tortor volutpat nisi, et consectetur tellus nunc eu turpis. Morbi rhoncus at nisi sed eleifend. Vivamus finibus nibh ipsum, et malesuada est ullamcorper nec. Phasellus pellentesque arcu at nunc tincidunt, id hendrerit arcu varius. Donec imperdiet condimentum lacus, ac maximus tortor tempus sit amet. Mauris tempor turpis ac sapien euismod feugiat. Nam at dolor a augue feugiat scelerisque non in sem. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris at est lacus. Nulla hendrerit, lectus ut vestibulum imperdiet, est dolor aliquet libero, at gravida magna lectus nec metus. Nam vitae finibus ante. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+
+Maecenas ullamcorper condimentum gravida. Etiam efficitur ut mi at consequat. Nullam eu magna quis dui maximus vehicula. Ut semper, leo ut sagittis volutpat, leo felis suscipit risus, nec laoreet elit quam ac sapien. Donec vel purus mauris. Vivamus urna orci, porta sit amet odio eu, scelerisque posuere arcu. Donec dignissim magna vel rhoncus pulvinar. 
+
+                    </p>
+                </div>
+                <div class="image_droite-blog-unit">
+                    <img src="assets/jpg/images page d'accueil/Villa-grande-Anse-Martinique02.jpg" alt="Photo Maison Droite">
+                </div>
+            </div>
+        </div>
+
+
+</main>
+
+<?php
+    include "includes/cursor.php";
+    include("./includes/footer.php"); 
+    ?>
---
+++ blog.php
@@ -0,0 +1,67 @@
+<?php
+require_once 'core/core.php';
+include 'includes/header.php';
+
+$select = $db->query("SELECT * FROM products");
+?>
+
+<main>
+
+ <!-- Section accueil -->
+ <div class="qhero_page_produits">
+        <div class="qhero_entree_page_produits">
+            <div class="div_h1_titre">
+                <h1 class="h1_titre">
+                    Notre Blog
+                </h1>
+            </div>
+            <div class="div_h3_text_presentation_produits">
+                <h3 class="h3_titre">
+                    Sed rutrum vulputate dapibus. Vivamus id tincidunt eros, in suscipit quam. Praesent aliquet justo auctor urna feugiat, luctus aliquam odio pharetra. Fusce iaculis mauris sem. Donec varius urna libero, sit amet tincidunt enim laoreet ut. Donec non sem pharetra, ullamcorper nibh in, lacinia risus. Proin rutrum egestas massa, a cursus diam interdum ac.
+                </h3>
+            </div>
+        </div>
+    </div>
+
+    <!-- Section boutons de filtre -->
+
+    <div class="section_boutons_filtre">
+        <div class="boutons_maisons">
+            <button class="bouton_selection_maison"><a href="">Martinique</a></button>
+            <button class="bouton_selection_maison"><a href="">Pyrénnées</a></button>
+            <button class="bouton_selection_maison"><a href="">Gruissan</a></button>
+            <button class="bouton_selection_maison"><a href="">Narbonne</a></button>
+        </div>
+        <div class="bouton_articles">
+            <button class="bouton_selection_maison bouton_agrandie"><a href="">Tous nos Articles</a></button>
+        </div>
+    </div>
+
+    <!-- Section produits -->
+
+    <div class="espace_flexbox_produit_02">
+    <?php while ($product = mysqli_fetch_assoc($select)) : ?>
+        <div class="espace_produit_02">
+            <a href="">
+                <img src="<?= $product['photo_p']; ?>" alt="Thumbnail du produit">
+            </a>
+            <a href="">
+                <h6><?= $product['price_p']; ?></h6>
+            </a>
+            <a href="">
+                <h5><?= $product['name_p']; ?></h5>
+            </a>
+            <a href="" class="lien_achat">Acheter →</a>
+        </div>
+        <?php endwhile; ?>
+    </div>
+
+
+
+</main>
+
+
+
+<?php
+    include "includes/cursor.php";
+    include("./includes/footer.php"); ?>
