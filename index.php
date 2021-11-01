738124120b43eb19c29aac41d214d9fd6e528f7d
jeanvay <40472316+jeanvay@users.noreply.github.com>
2021-11-01 21:33:00 +0100

    AOS de partout

--- index.php
+++ index.php
@@ -79,11 +79,11 @@ $sql = $db->query("SELECT * FROM rooms LIMIT 4");
 
   <!--  ######Ceci est la partie Html de la page d'accueil. Raccorder le back par la suite.##### -->
   <div class="qhero">
-    <div class="qhero_page_accueil" data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
-      <h1 class="style_h1_accueil"> “Laissez-vous séduire par un univers d’élégance <br> et de douceur.<br> 4 maisons, 4 ambiances le
+    <div class="qhero_page_accueil">
+      <h1 class="style_h1_accueil" data-aos="fade-up" data-aos-anchor-placement="bottom-bottom" data-aos-duration="1000" data-aos-delay="0"> “Laissez-vous séduire par un univers d’élégance <br> et de douceur.<br> 4 maisons, 4 ambiances le
       <br> regard d’une femme”</h1>
-      <h2 class="style_h2_accueil" data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">Je veux créer des maisons pour faire rêver, des maisons qui soient comme des personnalités, surprenantes, inattendues, accueillantes et généreuses. Où l’on ait envie de rester et de revenir, comme pour poursuivre une conversation interrompue trop tôt. </h2>
-      <div data-aos="fade-up"data-aos-anchor-placement="top-bottom">
+      <h2 class="style_h2_accueil" data-aos="fade-up" data-aos-anchor-placement="bottom-bottom" data-aos-duration="1000" data-aos-delay="200">Je veux créer des maisons pour faire rêver, des maisons qui soient comme des personnalités, surprenantes, inattendues, accueillantes et généreuses. Où l’on ait envie de rester et de revenir, comme pour poursuivre une conversation interrompue trop tôt. </h2>
+      <div data-aos="fade-up" data-aos-anchor-placement="bottom-bottom" data-aos-duration="1000" data-aos-delay="400">
         <svg width="40px" viewBox="0 0 14.334 24.75" class="scroll_anim">
         <circle class="circle-1" fill="black" cx="7.167" cy="6" r="1.2" />
         <circle class="circle-2" fill="black" cx="7.167" cy="6" r="1.2" />
@@ -96,8 +96,7 @@ $sql = $db->query("SELECT * FROM rooms LIMIT 4");
 
 
   <!-- Swiper -->
-  <div data-aos="fade-up"
-     data-aos-anchor-placement="top-bottom"
+  <div
       class="swiper mySwiper swiperHover">
     <div class="swiper-wrapper">
       <?php while ($room = $sql->fetch(PDO::FETCH_ASSOC)) :
@@ -112,9 +111,9 @@ $sql = $db->query("SELECT * FROM rooms LIMIT 4");
         <div class="swiper-slide">
           <img src="<?= $room['photo']; ?>" alt="" srcset="">
           <div class="swiper-slide-titre">
-            <h2 data-aos="fade-up"><?= $room['shortName']; ?></h2>
-            <p data-aos="fade-up"data-aos-anchor-placement="bottom-bottom"><?= $shortDesc . '...'; ?></p>
-            <button data-aos="fade-up" data-aos-anchor-placement="bottom-bottom"><a href="maison.php?room=<?= $room['id']; ?>">Découvrir</a></button>
+            <h2 data-aos="fade-up" data-aos-anchor-placement="bottom-bottom" data-aos-duration="1000" data-aos-delay="0"><?= $room['shortName']; ?></h2>
+            <p data-aos="fade-up" data-aos-anchor-placement="bottom-bottom" data-aos-duration="1000" data-aos-delay="200"><?= $shortDesc . '...'; ?></p>
+            <button data-aos="fade-up" data-aos-anchor-placement="bottom-bottom" data-aos-duration="1000" data-aos-delay="400"><a href="maison.php?room=<?= $room['id']; ?>">Découvrir</a></button>
           </div>
         </div>
       <?php endwhile; ?>
@@ -130,13 +129,11 @@ $sql = $db->query("SELECT * FROM rooms LIMIT 4");
 
   <div class="section_muriel">
     <div>
-      <img src="assets/jpg/Muriel.png" alt="Photo Profil Muriel" class="img_muriel" data-aos="fade-up"
-     data-aos-anchor-placement="center-bottom"> 
+      <img src="assets/jpg/Muriel.png" alt="Photo Profil Muriel" class="img_muriel" data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-duration="1000" data-aos-delay="100"> 
     </div>
     <div class="section_muriel_titre_text">
-      <h2 class="titre_section_muriel" data-aos="fade-up"
-     data-aos-anchor-placement="bottom-bottom">Muriel</h2>
-      <p class="texte_section_muriel" data-aos="fade-up">Je quittai à 20 ans la métropole pour la Martinique, par amour et par désir d’aventures. Avec mon compagnon, nous sommes arrivés là-bas avec nos seules valises et envies de conquérir le monde. De débrouilles en petits boulots, incertains du lendemain, nous avons finalement passé 21 ans sur cette île aux mille couleurs et saveurs. Inspirée par cette terre où se rencontrent les cultures, où se métissent les imaginations, je me suis prise d’une véritable passion pour habiller les murs de ma maison de toutes ces influences.<br> <br>
+      <h2 class="titre_section_muriel" data-aos="fade-up" data-aos-anchor-placement="bottom-bottom" data-aos-duration="1000" data-aos-delay="200">Muriel</h2>
+      <p class="texte_section_muriel" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="1000" data-aos-delay="500">Je quittai à 20 ans la métropole pour la Martinique, par amour et par désir d’aventures. Avec mon compagnon, nous sommes arrivés là-bas avec nos seules valises et envies de conquérir le monde. De débrouilles en petits boulots, incertains du lendemain, nous avons finalement passé 21 ans sur cette île aux mille couleurs et saveurs. Inspirée par cette terre où se rencontrent les cultures, où se métissent les imaginations, je me suis prise d’une véritable passion pour habiller les murs de ma maison de toutes ces influences.<br> <br>
         Quand j’ai eu envie d’explorer d’autres horizons, Narbonne fut comme une évidence. J’y retrouvais le soleil, la mer, la générosité joyeuse de ses habitants. Rien ne me plaît tant que le contraste. J’aime chiner dans les greniers et les brocantes, trouver des meubles auxquels redonner vie. Je ponce, je peins, je détourne. J’aime cette petite étincelle d’étonnement que provoque la rencontre entre un objet design et une bricole restaurée sur mes murs.<br><br>
         Je veux créer des maisons pour faire rêver, des maisons qui soient comme des personnalités, surprenantes, inattendues, accueillantes et généreuses. Où l’on ait envie de rester et de revenir, comme pour poursuivre une conversation interrompue trop tôt.son de charme situé au coeur de la martinique. </p>
     </div>
