8ffee5216d19239bd332310eb66028d3cc59e6b2
jeanvay <40472316+jeanvay@users.noreply.github.com>
2021-11-07 22:18:03 +0100

    nuance si safari no cursor

--- includes/cursor.php
+++ includes/cursor.php
@@ -2,9 +2,21 @@
 <div class="cursor2"></div>
 
 <script>
+
+  var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
   var cursor = document.querySelector(".cursor");
   var cursor2 = document.querySelector(".cursor2");
+
+  if(isSafari){ 
+  /*alert('Is safari');*/
+  //Do something
+
+} else {
+  /*alert('Is other browser');*/
+
   document.addEventListener("mousemove", function(e) {
     cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
   });
+}
 </script>
