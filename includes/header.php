3b323811689ae01566460cfad9a4a911615019ba
iamthesouthvibe <abc.studiodecreation@gmail.com>
2021-11-09 19:30:41 +0100

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,7 @@
   <meta name="author" content="">
   <title>MH My Home</title>
   <!-- Custom styles for this template -->
-  <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+  <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
 
   <link rel="stylesheet" href="../style/style.css">
 
@@ -19,19 +19,19 @@
 
 <body>
 
-<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+  <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
 
   <header data-aos="fade" data-aos-anchor-placement="bottom-bottom" data-aos-duration="1000">
-    
+
     <nav>
       <div class="header_left">
-        <a  class="waitBeforeNavigate" href="index.php">
-        <img src="assets/png/LOGO_02_PNG_NOIR.png" alt="Logo muriel Home">
+        <a class="waitBeforeNavigate" href="index.php">
+          <img src="assets/png/LOGO_02_PNG_NOIR.png" alt="Logo muriel Home">
         </a>
       </div>
       <div class="header_right">
-        <button class="bouton_desktop" id="bouton_desktop"><a   href="reservation.php" class="waitBeforeNavigate">Réserver</a></button>
-        <div  class="burger-menu">
+        <button class="bouton_desktop" id="bouton_desktop"><a href="reservation.php" class="waitBeforeNavigate">Réserver</a></button>
+        <div class="burger-menu">
           <span></span>
           <span></span>
           <span></span>
@@ -40,7 +40,7 @@
     </nav>
 
     <div class="bouton_responsive" id="bouton_responsive">
-      <button class="bouton_phone"><a class="waitBeforeNavigate"  href="reservation.php">Réserver</a></button>
+      <button class="bouton_phone"><a class="waitBeforeNavigate" href="reservation.php">Réserver</a></button>
     </div>
 
   </header>
@@ -50,81 +50,82 @@
       <div class="menu_nav_link">
         <ul>
           <li>
-             <a class="waitBeforeNavigate"  href="index.php" >Home</a>
+            <a class="waitBeforeNavigate" href="index.php">Home</a>
           </li>
           <li>
-            <a class="waitBeforeNavigate"  href="blog.php">Rubriques</a>
+            <a class="waitBeforeNavigate" href="blog.php">Rubriques</a>
           </li>
+          <!--
           <li>
             <a class="waitBeforeNavigate"  href="products.php">Produits</a>
           </li>
+          -->
           <li>
-            <a class="waitBeforeNavigate"  href="contact.php">Contact</a>
+            <a class="waitBeforeNavigate" href="contact.php">Contact</a>
           </li>
         </ul>
       </div>
     </div>
     <div class="menu_nav_right">
-    <ul>
-          <li>
-            <a class="waitBeforeNavigate"  href="index.php">01</a>
-          </li>
-          <li>
-            <a class="waitBeforeNavigate"  href="">02</a>
-          </li>
-          <li>
-            <a class="waitBeforeNavigate"  href="">03</a>
-          </li>
-          <li>
-            <a class="waitBeforeNavigate"  href="contact.php">04</a>
-          </li>
-        </ul>
+      <ul>
+        <li>
+          <a class="waitBeforeNavigate" href="index.php">01</a>
+        </li>
+        <li>
+          <a class="waitBeforeNavigate" href="blog.php">02</a>
+        </li>
+        <!-- <li>
+          <a class="waitBeforeNavigate" href="">03</a>
+        </li> -->
+        <li>
+          <a class="waitBeforeNavigate" href="contact.php">04</a>
+        </li>
+      </ul>
     </div>
   </div>
 
 </body>
 
-  <script>
-    let navBar = document.querySelector('.menu_nav');
-    let navBarRight = document.querySelector('.menu_nav_right');
-    let menuBurger = document.querySelector('.burger-menu');
-    let headerButton = document.querySelector('.bouton_responsive')
-    menuBurger.addEventListener("click", (event) => {
-      if (event.currentTarget.classList.contains("open")) {
-        event.currentTarget.classList.remove("open");
-        navBar.classList.remove("active");
-        navBarRight.classList.remove("active_snd");
-        headerButton.style.display = ""
-        console.log(headerButton)
-        document.body.style.overflow = ""
-      } else {
-        event.currentTarget.classList.add("open");
-        navBar.classList.add("active");
-        navBarRight.classList.add("active_snd");
-        headerButton.style.display = "none"
-        document.body.style.overflow = "hidden"
-      }
-    });
-  </script>
+<script>
+  let navBar = document.querySelector('.menu_nav');
+  let navBarRight = document.querySelector('.menu_nav_right');
+  let menuBurger = document.querySelector('.burger-menu');
+  let headerButton = document.querySelector('.bouton_responsive')
+  menuBurger.addEventListener("click", (event) => {
+    if (event.currentTarget.classList.contains("open")) {
+      event.currentTarget.classList.remove("open");
+      navBar.classList.remove("active");
+      navBarRight.classList.remove("active_snd");
+      headerButton.style.display = ""
+      console.log(headerButton)
+      document.body.style.overflow = ""
+    } else {
+      event.currentTarget.classList.add("open");
+      navBar.classList.add("active");
+      navBarRight.classList.add("active_snd");
+      headerButton.style.display = "none"
+      document.body.style.overflow = "hidden"
+    }
+  });
+</script>
 
 <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
-  <script>
-    AOS.init();
-  </script>
+<script>
+  AOS.init();
+</script>
 
 <script>
   function waitBeforeNavigate(ev) {
     ev.preventDefault(); // prevent default anchor behavior
     const goTo = this.getAttribute("href"); // store anchor href
 
-    setTimeout(function () {
-        window.location = goTo;
+    setTimeout(function() {
+      window.location = goTo;
     }, 1000); // time in ms
 
     document.body.style.opacity = "0"
-};
+  };
 
-document.querySelectorAll(".waitBeforeNavigate")
+  document.querySelectorAll(".waitBeforeNavigate")
     .forEach(EL => EL.addEventListener("click", waitBeforeNavigate));
-      
 </script>
