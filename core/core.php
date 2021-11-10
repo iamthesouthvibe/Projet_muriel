ed4eb8055339c7ace0b857519a148b2554102598
iamthesouthvibe <abc.studiodecreation@gmail.com>
2021-11-10 22:20:34 +0100

    Update core.php

--- core/core.php
+++ core/core.php
@@ -1,5 +1,7 @@
 <?php
-$db = new PDO('mysql:host=localhost;dbname=hotel_db', 'root', 'root');
+// $db = new PDO('mysql:host=localhost;dbname=hotel_db', 'root', 'root');
+$db = new PDO('mysql:host=node67-eu.n0c.com;dbname=sxgdzjcq_testSQL', 'sxgdzjcq', '2A8zptPYcmZuQH');
+
 
 if (!$db) {
     echo 'Could not establish database connection, please review your settings';
