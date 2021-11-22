21a37e374c7b7d0883d2c6937a32084de29e4ed4
iamthesouthvibe <abc.studiodecreation@gmail.com>
2021-11-22 20:13:10 +0100

    Update fonctionMail.php

--- fonctions/fonctionMail.php
+++ fonctions/fonctionMail.php
@@ -4,9 +4,9 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\SMTP;
 
-require '../vendor/phpmailer/phpmailer/src/Exception.php';
-require '../vendor/phpmailer/phpmailer/src/PHPMailer.php';
-require '../vendor/phpmailer/phpmailer/src/SMTP.php';
+require './vendor/phpmailer/phpmailer/src/Exception.php';
+require './vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require './vendor/phpmailer/phpmailer/src/SMTP.php';
 
 
 function sendMail($message)
